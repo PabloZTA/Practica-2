8bde3e5a6490ad7edc7549f8f9d96972b0b9d249
Hurt9901 <brayanroco99@gmail.com>
2024-04-03 18:57:08 -0500

    Entidad ActividadInvestigacion

---
+++ Investigation.Shared/Entities/ActividadesInvestigacion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigation.Shared.Entities
+{
+    public class ActividadesInvestigacion
+    {
+        public int Id { get; set; }
+
+
+        [Display(Name = "Nombre: ")]
+        [MaxLength(100, ErrorMessage = "No se permiten más de 50 Caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Nombre { get; set; }
+
+
+
+        [Display(Name = "Descripción")]
+        [MaxLength(100, ErrorMessage = "No se permiten más de 100 Caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Descripcion { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+
+        public DateTime FechaInicio { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+
+        public DateTime FechaFinal { get; set; }
+
+    }
+}
