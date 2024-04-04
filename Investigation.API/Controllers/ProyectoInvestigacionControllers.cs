1821d7d260dda17124db7873e22c10d52a33e322
PabloZTA <RYZEN 5@PABLOZTA>
2024-04-03 20:47:52 -0500

    controladores proyecto investigacion y actividades investigacion

---
+++ Investigation.API/Controllers/ProyectoInvestigacionControllers.cs
@@ -0,0 +1,72 @@
+﻿using Investigation.API.Data;
+using Investigation.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Investigation.API.Controllers
+{
+    [ApiController]
+    [Route("/api/Proyectoinvestigacion")]
+    public class ProyectoInvestigacionControllers:ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ProyectoInvestigacionControllers(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.ActividadesInvestigacion.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(ActividadesInvestigacion actividadesInvestigacion)
+        {
+            _context.Add(actividadesInvestigacion);
+            await _context.SaveChangesAsync();
+            return Ok(actividadesInvestigacion);
+        }
+
+        [HttpGet("id:int")]
+        public async Task<ActionResult> Get(int id)
+        {
+
+            var actividadesInvestigacion = await
+                _context.ActividadesInvestigacion.FirstOrDefaultAsync(x => x.Id
+                == id);
+            if (actividadesInvestigacion == null)
+            {
+                return NotFound();
+            }
+            return Ok(actividadesInvestigacion);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(ActividadesInvestigacion actividadesInvestigacion)
+        {
+            _context.Add(actividadesInvestigacion);
+            await _context.SaveChangesAsync();
+            return Ok(actividadesInvestigacion);
+        }
+
+        [HttpDelete("id:int")]
+        public async Task<ActionResult> Delete(int id)
+        {
+
+            var Filasafectadas = await _context.ActividadesInvestigacion
+
+                .Where(x => x.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (Filasafectadas == 0)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
