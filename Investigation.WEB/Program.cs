27b3f0a6485529458e9b9786fd4d4ec8a6dfa5ca
PABLOZTA <PABLOZTA@PABLOZTA>
2024-05-01 16:00:19 -0500

    prueba

--- Investigation.WEB/Program.cs
+++ Investigation.WEB/Program.cs
@@ -1,7 +1,9 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Investigation.WEB;
 using Investigation.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000") });
 
+builder.Services.AddSweetAlert2();
 
 builder.Services.AddScoped<IRepository, Repository>();
 
