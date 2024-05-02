6e3cd7d946faa7029c14755d19c78f1cef0d087c
Hurt9901 <brayanroco99@gmail.com>
2024-05-01 20:43:21 -0500

    Investigadores

--- Investigation.WEB/Repositories/IRepository.cs
+++ Investigation.WEB/Repositories/IRepository.cs
@@ -5,10 +5,15 @@ namespace Investigation.WEB.Repositories
     public interface IRepository
     {
         Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
-        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
-        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
-        Task<HttpResponseWrapper<object>> DeleteAsync(string url);
+
+        Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model);
+
+        Task<HttpResponseWrapper<object>> DeleteAsync<T>(string url);
+
         Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model);
-        Task<HttpResponseWrapper<TResponse>> PutAsync<T, TResponse>(string url, T model);
+
+        Task<HttpResponseWrapper<TActionResponse>> PutAsync<T, TActionResponse>(string url, T model);
     }
 }
